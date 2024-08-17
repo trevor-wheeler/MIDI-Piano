279f25cdbe40b293010c1cea2f9463f9b98276ac
Trevor Wheeler <trevoraustinwheeler@gmail.com>
2024-08-17 02:04:52 -0700

    Fix theme localStorage bug

--- midi/piano/static/piano/scripts/main.js
+++ midi/piano/static/piano/scripts/main.js
@@ -173,19 +173,25 @@ function closePopup() {
 }
 
 function getTheme() {
-    let theme;
+    let theme = localStorage.getItem('theme');
 
-    // If default theme is dark set local storage to dark
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        localStorage.setItem('theme', 'dark');
-        theme = 'dark';
-    }
-    // If default theme is light set local storage to light
-    else {
-        localStorage.setItem('theme', 'light');
-        theme = 'light';
+    const getThemePreference = () => {
+        // If default theme is dark set local storage to dark
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            localStorage.setItem('theme', 'dark');
+            theme = 'dark';
+        }
+        // If default theme is light set local storage to light
+        else {
+            localStorage.setItem('theme', 'light');
+            theme = 'light';
+        }
     }
 
+    // Check for theme in local storage
+    if (!theme || theme && theme === 'auto') {
+        getThemePreference();
+    }
 
     // Update body to reflect theme
     document.body.classList.add(theme);
